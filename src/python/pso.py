e2cecc48b805cff43547f1a5470284b50ac8e146
sueskind <52210599+sueskind@users.noreply.github.com>
2023-05-23 14:33:59 +0200

    Move Python PSO

--- src/python/pso.py
+++ src/python/pso.py
@@ -1,6 +1,6 @@
 from p5 import *
 
-from lib.pso import pso_iter
+from python.lib import pso_iter
 
 s = 100
 
@@ -11,12 +11,12 @@ def square(xi: np.ndarray) -> float:
     return float(np.sum(xi ** 2))
 
 
-iterator = pso_iter(square, dim=2, n=20, s=100, a=0.9, b_loc=0.3, b_glob=0.3, c=1, d=1)
+iterator = pso_iter(square, dim=2, n=10, s=s, a=0.99, b_loc=0.1, b_glob=0.1, c=1, d=1)
 
 
 def setup():
     size(700, 700)
-    stroke_weight(5)
+    stroke_weight(1)
     stroke(0)
     fill(0)
 
@@ -24,12 +24,11 @@ def setup():
 def draw():
     background(255)
     translate(width / 2, height / 2)
+    scale(7)
 
     x = next(iterator)
     for xi in x:
         point(*xi)
-
-    time.sleep(0.01)
 
 
 if __name__ == "__main__":
